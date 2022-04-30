83c3b718aa390022a9ecbf8b06205f5c6f034b85
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-05-01 00:41:36 +0300

    Pass Postman tests

---
+++ postgres_to_es/get_data_psql.py
@@ -0,0 +1,153 @@
+import logging
+
+from elasticsearch import helpers
+from psycopg2.extensions import connection as _connection
+
+import my_connection
+import json
+from my_connection import *
+
+BULK = 10
+
+def create_tables_list():
+    tables_list = list()
+    tables_list.append('film_work')
+
+    return tables_list
+
+
+def get_data_from_table(pg_conn:_connection, table:str):
+
+    if table == 'film_work':
+        try:
+            with pg_conn.cursor() as p_curs:
+
+                # query_text = '''
+                #     SELECT json_agg(movies_data)
+                #         FROM
+                #         (SELECT
+                #             fw.id,
+                #             fw.title,
+                #             fw.description,
+                #             fw.rating as imdb_rating,
+                #             p.full_name as director
+                #         FROM content.film_work fw
+                #         LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+                #         LEFT JOIN content.person p ON p.id = pfw.person_id
+                #         WHERE pfw.role = 'director'
+                #         ORDER BY fw.updated_at
+                #         LIMIT 10) movies_data;
+                #     '''
+
+                # json_agg(DISTINCT
+                # p.full_name
+                # )
+                # FILTER(WHERE
+                # pfw.role = 'director')
+                query_text = """
+                    SELECT json_agg(movies_data)
+                    FROM (
+                        SELECT
+                            fw.id as _id,
+                            fw.id,
+                            fw.title,
+                            fw.description,
+                            fw.rating as imdb_rating,
+                            COALESCE(
+                                json_agg(
+                                    DISTINCT p.full_name
+                                    ) FILTER (WHERE pfw.role = 'director'),
+                                '[]')
+                            as director,
+                            COALESCE (
+                                json_agg(
+                                    DISTINCT jsonb_build_object(
+                                   'id', p.id,
+                                   'name', p.full_name
+                                    )
+                                ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+                                '[]'
+                            ) as actors,
+                            COALESCE (
+                                json_agg(
+                                    DISTINCT p.full_name
+                                    ) FILTER (WHERE p.id is not null AND pfw.role = 'actor'),
+                                '[]'
+                            ) as actors_names,
+                            COALESCE (
+                                json_agg(
+                                    DISTINCT jsonb_build_object(
+                                   'id', p.id,
+                                   'name', p.full_name
+                                    )
+                                ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+                                '[]'
+                            ) as writers,
+                            COALESCE (
+                                json_agg(
+                                    DISTINCT p.full_name
+                                    ) FILTER (WHERE p.id is not null AND pfw.role = 'writer'),
+                                '[]'
+                            ) as writers_names,
+                            array_agg(DISTINCT g.name) as genre
+                        FROM content.film_work fw
+                        LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+                        LEFT JOIN content.person p ON p.id = pfw.person_id
+                        LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+                        LEFT JOIN content.genre g ON g.id = gfw.genre_id
+                      
+                        GROUP BY fw.id
+                        ORDER BY fw.updated_at
+                        
+                        ) movies_data;
+                """
+
+                p_curs.execute(query_text)
+                data = p_curs.fetchall()
+                with open('movies.json', 'w') as movies_data:
+                    json.dump(data[0][0], movies_data)
+                send_data_to_es()
+
+        except psycopg2.Error as error:
+            logging.error('Ошибка чтения таблицы ', table, error)
+
+
+def send_data_to_es():
+    es_client = my_connection.connect_to_esl()
+    if es_client.ping():
+        index_name = 'movies'
+        index_created = create_index(es_client, index_name)
+        if index_created:
+            try:
+                with open('movies.json', 'r') as movies_data:
+                    bulk_data = json.load(movies_data)
+                response = helpers.bulk(es_client, bulk_data, index=index_name)
+                logging.info(' '.join(('Bulk', str(response[0]), 'documents')))
+            except Exception as e:
+                logging.error(e.args[0])
+
+
+def create_index(es_object, index_name):
+    created = False
+    with open('index_settings.json', 'r') as ind_set:
+        settings = json.load(ind_set)
+    try:
+        if not es_object.indices.exists(index=index_name):
+            es_object.indices.create(index=index_name, ignore=400, body=settings)
+            logging.info(' '.join(('Created Index', index_name)))
+        created = True
+    except Exception as ex:
+        logging.error(ex)
+    finally:
+        return created
+
+
+def load_from_psql(pg_conn: _connection):
+    tables_list = create_tables_list()
+
+    for table_name in tables_list:
+        get_data_from_table(pg_conn, table_name)
+
+
+if __name__ == '__main__':
+    connect_to_db()
---
+++ postgres_to_es/my_connection.py
@@ -0,0 +1,50 @@
+import logging
+import os
+import psycopg2
+
+from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
+from get_data_psql import load_from_psql
+from psycopg2.extras import DictCursor
+
+
+def get_environment_var():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    env_var = {'db_name': os.environ.get('DB_NAME'), 'user': os.environ.get('USER_APP'),
+               'password': os.environ.get('PASSWORD'), 'host': os.environ.get('HOST'), 'port': os.environ.get('PORT'),
+               'db_path': os.environ.get('DB_PATH'),
+               'elastic_pass': os.environ.get('ELASTIC_PASSWORD'),
+                }
+
+    return env_var
+
+
+def connect_to_db():
+
+    env_var = get_environment_var()
+
+    dsl = {'dbname': env_var['db_name'],
+           'user': env_var['user'],
+           'password': env_var['password'],
+           'host': env_var['host'],
+           'port': env_var['port']}
+
+    db_path = env_var['db_path']
+
+    logging.basicConfig(filename='loading.log', filemode='w')
+    logging.root.setLevel(logging.NOTSET)
+
+    with psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+        load_from_psql(pg_conn)
+    pg_conn.close()
+
+
+def connect_to_esl():
+    elast_pass = get_environment_var()['elastic_pass']
+    es = Elasticsearch("http://127.0.0.1:9200",
+                       basic_auth=("elastic", elast_pass)
+                       )
+    logging.info(es.info())
+    return es
