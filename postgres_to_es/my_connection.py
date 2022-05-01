1c5cb2c73b27fa9e9c83514500b95fa5ecc56b9c
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-05-01 23:12:49 +0300

    Add: backoff

--- postgres_to_es/my_connection.py
+++ postgres_to_es/my_connection.py
@@ -1,11 +1,45 @@
 import logging
 import os
+import time
+
 import psycopg2
 
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+from functools import wraps
 from get_data_psql import load_from_psql
 from psycopg2.extras import DictCursor
+
+
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
+    """
+    Функция для повторного выполнения функции через некоторое время, если возникла ошибка. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time)
+
+    Формула:
+        t = start_sleep_time * 2^(n) if t < border_sleep_time
+        t = border_sleep_time if t >= border_sleep_time
+    :param start_sleep_time: начальное время повтора
+    :param factor: во сколько раз нужно увеличить время ожидания
+    :param border_sleep_time: граничное время ожидания
+    :return: результат выполнения функции
+    """
+
+    def func_wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            t = start_sleep_time
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except:
+                    if t >= border_sleep_time:
+                        t = border_sleep_time
+                    else:
+                        t = start_sleep_time * 2 ^ factor
+                    time.sleep(t)
+        return inner
+
+    return func_wrapper
 
 
 def get_environment_var():
@@ -21,6 +55,7 @@ def get_environment_var():
     return env_var
 
 
+@backoff()
 def connect_to_db():
     env_var = get_environment_var()
 
@@ -45,3 +80,6 @@ def connect_to_esl():
                        )
     logging.info(es.info())
     return es
+
+
+
